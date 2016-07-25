ad818fc757418403627d26e92f6514236f1dd713
jcursoli <joeycursoli2@gmail.com>
2016-07-25 16:54:34 -0700

    [add] adds landing page and sets up video background

--- src/components/landingComponent.js
+++ src/components/landingComponent.js
@@ -5,9 +5,17 @@ class Landing extends Component{
 	render(){
 		return (
 			<div>
-				<h1>
-					LANDING
-				</h1> 
+				<video autoPlay poster="https://i.vimeocdn.com/video/533883117_640x360.jpg" id="bgvid" loop>
+				<source src="https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us/01/2700/5/138504815/412314518.mp4?token=5796ddbd_0x041fa50a903503427870251976b93bf1bea80d6c" type="video/mp4" />
+				</video>
+				<div className='row center-xs'>
+					<div className='col-xs-12 col-sm-8 col-md-6 col-lg-4'>
+						<div className='content'>
+							<div className='landing'>Shux</div>
+							<div className='intro'>The Place to Meet People Like You</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
